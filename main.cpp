2d94f92d1c6564381191112c6018cb459297001c
Lemos <86426372+lemoslemos@users.noreply.github.com>
2022-08-26 13:47:03 -0300

    added template

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
+#include <cstdlib>
 #include <cmath>
+#include "data.hpp"
+#include "hungarian.h"
 
 /**
  * The upper bound is defined as the cost of a valid TSP solution
@@ -98,3 +102,33 @@ Node root;
 // calculate solution using hungarian.cpp and update `root`
 
 tree.push_back(root);
+
+int main (int argc, char **argv) {
+	Data *data = new Data(argc, argv[1]);
+	data->readData();
+
+	double **cost = new double*[data->getDimension()];
+	for (int i = 0; i < data->getDimension(); i++){
+		cost[i] = new double[data->getDimension()];
+		for (int j = 0; j < data->getDimension(); j++){
+			cost[i][j] = data->getDistance(i,j);
+		}
+	}
+
+	hungarian_problem_t p;
+	int mode = HUNGARIAN_MODE_MINIMIZE_COST;
+	hungarian_init(&p, cost, data->getDimension(), data->getDimension(), mode); // Carregando o problema
+
+	double obj_value = hungarian_solve(&p);
+	cout << "Obj. value: " << obj_value << endl;
+
+	cout << "Assignment" << endl;
+	hungarian_print_assignment(&p);
+
+	hungarian_free(&p);
+	for (int i = 0; i < data->getDimension(); i++) delete [] cost[i];
+	delete [] cost;
+	delete data;
+
+	exit(EXIT_SUCCESS);
+}
